607e947332c12836069c3e9c121cfcac363fee47
louis <louisasare@hotmail.com>
2022-02-09 00:35:10 +0000

    q1 first attempt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
+class App extends Component {
+
+  state = {
+    inputText : [
+      {lengthOfInput : 0}
+    ]
+  }
+
+  countLength = (event) =>{
+    let array = {...event.target.value};
+
+    let lengthOfText = array.forEach(el => {
+      let count = 0;
+      count++;
+
+      return count
+    })
+    let newLength = {lengthOfInput : lengthOfText}
+    
+    this.setState({inputText:newLength})
+  }
+
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <input OnChange={(event) => this.countLength}></input>
+     <p>{this.state.inputText.lengthOfInput}</p>
     </div>
   );
+
+  }
 }
 
 export default App;
+
+
+
+/// 1) Create an input field (in app component) with a change listener which outputs the length of the entered text below it(e.g. <p> tag) 
